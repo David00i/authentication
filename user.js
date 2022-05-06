5e137c0ee9b2511bd71b2714c44ef8ae64d63e5d
David 00i <ooidavid10@gmail.com>
2022-05-06 20:10:51 +0800

    update

--- user.js
+++ user.js
@@ -2,10 +2,11 @@ let users;
 
 class User {
 	static async injectDB(conn) {
-		users = await conn.db("my-database-name").collection("users")
+		users = await conn.db("authentication").collection("users")
 	}
 
-	static async register(username, password) {
+	static async register(username) {
+		return username = await users.findOne({ username:"david" }, { projection: { _id: 0, username: 1 } })
 		// TODO: Check if username exists
 
 		// TODO: Hash password
@@ -13,14 +14,14 @@ class User {
 		// TODO: Save user to database
 	}
 
-	static async login(username, password) {
-		// TODO: Check if username exists
+	// static async login(username, password) {
+	// 	// TODO: Check if username exists
 
-		// TODO: Validate password
+	// 	// TODO: Validate password
 
-		// TODO: Return user object
-		return;
-	}
+	// 	// TODO: Return user object
+	// 	return;
+	// }
 }
 
 module.exports = User;
--- user.test.js
+++ user.test.js
@@ -1,11 +1,18 @@
 const MongoClient = require("mongodb").MongoClient;
 const User = require("./user")
 
+const testUser = {
+	"_id": "627109b9bce944eb9729d61d",
+	username: "david",
+	email: "david10@gmail.com",
+	password: "123456",
+  }
+
 describe("User Account", () => {
 	let client;
 	beforeAll(async () => {
 		client = await MongoClient.connect(
-			"my-mongodb+srv-connection-string",
+			"mongodb+srv://m001-student:m001-mongodb-basics@sandbox.te4xf.mongodb.net/Sandbox?retryWrites=true&w=majority",
 			{ useNewUrlParser: true },
 		);
 		User.injectDB(client);
@@ -16,27 +23,37 @@ describe("User Account", () => {
 	})
 
 	test("New user registration", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
+		const res = await User.register();
+		expect(res).toEqual({"username": "david"})
 	})
 
-	test("Duplicate username", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
-	})
+	// test("Duplicate username", async () => {
+	// 	const res = await User.register("test", "test")
+	// 	expect().toBe()
+	// })
 
-	test("User login invalid username", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
-	})
+	// test("User login invalid username", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect().toBe()
+	// })
 
-	test("User login invalid password", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
-	})
+	// test("User login invalid password", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect().toBe()
+	// })
 
-	test("User login successfully", async () => {
-		const res = await User.login("test", "test")
-		expect(res).toBe(true)
-	})
+	// test("User login successfully", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect(res).toBe(true)
+	// })
+
+	// test('Test for Part A+B', async () => {
+	// 	const res = await partab();
+	// 	expect(res.length).toBe(1);
+	// 	expect(res[0].accounts.length).toBe(4);
+	// 	expect(res[0].accounts[0]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e5");
+	// 	expect(res[0].accounts[1]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e3");
+	// 	expect(res[0].accounts[2]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e4");
+	// 	expect(res[0].accounts[3]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e6");
+	// });
 });
